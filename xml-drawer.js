62457779be876aae9612c415bda2a165a56f25ed
HunYan-io <hhheeennniii@gmail.com>
2019-02-24 16:50:37 +0100

    image height fix

--- xml-drawer.js
+++ xml-drawer.js
@@ -42,7 +42,7 @@ const drawRect = function(ctx, x, y, width, height, angle, style) {
 	ctx.translate(x,y);
 	ctx.rotate(angle);
 	if (style instanceof Canvas.Image) {
-		ctx.drawImage(style, -width/2, -height/2, width, parseInt(height)+1)
+		ctx.drawImage(style, -width/2, -height/2, width, parseInt(height)+2)
 	} else {
 		ctx.fillStyle = style;
 		ctx.fillRect(-width/2, -height/2, width, height);
@@ -77,7 +77,7 @@ const drawGround = function(bg, fg, ground) {
 			return [Canvas.loadImage("grounds/"+tiledGrounds[t][0]+".png"), (image) => {
 				drawRect(map,x,y,l,h,a,map.createPattern(image,"repeat"));
 				return Canvas.loadImage("grounds/"+tiledGrounds[t][1]+".png").then((top) => {
-					drawRect(map,x,y,l,top.height-2,a,map.createPattern(top,"repeat"));
+					drawRect(map,x,y,l,top.height-3,a,map.createPattern(top,"repeat"));
 				});
 			}];
 		} else {

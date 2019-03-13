abd955cb6dfda0db420c01acdaaae3e752478d8d
HunYan-io <hhheeennniii@gmail.com>
2019-03-13 19:52:59 +0100

    added D attribute

--- xml-drawer.js
+++ xml-drawer.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const { parseString } = require('xml2js');
 const resource = require('./resource');
 
+//TODO LIST <Z INDEX ATTRIBUTE><(TAG/SUBTAG): P/DS P/mc D/DC D/DC2 D/DS><O TAG SHAMAN OBJECTS>
+
 const tiledGrounds = {"3":true,"5":true,"6":["5","6"],"7":true,"9":true,"10":["10","10p"],"11":["5","11"],"15":true}
 const decorOrigins = [[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[1.35,1],[2,1],[2,1],[2,2],[2,1],[2,1],[2,1],[2,1],[2,1.162],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,2],[2,1],[2,1],[2.3,1.03],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[Infinity,Infinity],[2,2],[2,2],[2,1],[2,Infinity],[2,2],[2,1],[2,6.166],[2,1.028],[2,2],[2,1],[2,2],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1.028],[2,1],[2,2],[1.333,Infinity],[2,1],[24.9,5.833],[2,1],[Infinity,Infinity],[2,Infinity],[2,Infinity],[119,4],[2,1],[2,1],[Infinity,Infinity],[2,2],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[4,Infinity],[2,1],[2,1.1],[2,Infinity],[2,1],[3.8,1],[2,1],[2,2],[2,2],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,2],[2,2],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,Infinity],[2,1],[2,1],[2,1],[2,1],[Infinity,Infinity],[2,2],[2,Infinity],[Infinity,1],[1,1],[2,2],[2,1],[2,2],[2,1],[2,1],[Infinity,Infinity],[2,1],[2,1],[2,1],[2,2],[2,1],[2,Infinity],[2,1],[2,1],[4,1],[2,1],[2,1],[2,1],[2,1],[2,2],[2,1.26],[2,1.2]];
 const noStrokeGrounds = {"1":true,"8":true,"9":true,"15":true}
@@ -75,14 +77,15 @@ const drawCircle = function(ctx, x, y, r, style) {
 const drawGround = function(map, ground, foreground) {
 	if (ground.m==='') return;
 
-	let a = (ground.p || "").split(",")[4] || 0;
+	let p = (ground.p || "").split(","),
+		a = p[4] || 0;
 	let {x=0,y=0,l=10,h=10,o,t="0"} = ground;
 
 	if (!(ground.n || t == 9) == foreground) return;
 
 	a *= Math.PI/180;
 
-	const stroke = !noStrokeGrounds[t];
+	const stroke = (p[0]==='0') && !noStrokeGrounds[t];
 
 	if (t == 12) {
 		if (!o) return;
@@ -177,6 +180,19 @@ const drawDecoration = function(map, obj, foreground) {
 	}];
 }
 
+const drawImages = function(map, dAttr, order) {
+	if (!dAttr) return;
+
+	const images = dAttr.split(';');
+
+	for (i = 0; i < images.length; i++) {
+		let [url='', x=0, y=0] = images[i].split(',');
+		order.add([resource.loadImage('https://www.transformice.com/images/'+url), (image)=>{
+			map.drawImage(image,x,y,image.width,image.height);
+		}]);
+	}
+}
+
 const drawXml = function(xml) {
 	if (!xml.c || !xml.c.p || !xml.c.z || !xml.c.z[0]) throw error("Invalid tfm map xml format.");
 	
@@ -189,16 +205,20 @@ const drawXml = function(xml) {
 
     const [canvas, map] = resource.createCanvas(width, height);
 
+	map.fillStyle = "#6A7495";
+	map.fillRect(0,0,width,height);
+	
 	const grounds = xml.c.z[0].s[0].s || [],
  		  joints = (xml.c.z[0].l || [''])[0].$$ || [],
  		  decorations = xml.c.z[0].d[0].$$ || [];
 
-	if (!propreties.f) {
-		map.fillStyle = "#6A7495";
-		map.fillRect(0,0,width,height);
-	} else {
-		order.add([resource.load("/backgrounds/"+propreties.f, canvas), ()=>{}]);
-	}
+	//background images
+	drawImages(map, propreties.dd, order);
+
+	//background
+	if (propreties.f)
+		order.add([resource.load("/backgrounds/"+propreties.f, canvas), f=>f()]);
+
 	//background decorations
 	for (let i = decorations.length-1; i > -1; i--)
 		order.add(drawDecoration(map, decorations[i], null));
@@ -222,6 +242,8 @@ const drawXml = function(xml) {
 	//foreground grounds
 	for (let i = 0; i < grounds.length; i++)
 		order.add(drawGround(map, grounds[i].$, true));
+	//foreground images
+	drawImages(map, propreties.d, order);
 
 	return order.onFinish(() => {
 		return canvas
@@ -229,8 +251,9 @@ const drawXml = function(xml) {
 };
 
 module.exports = function(xmlString,callback) {
+	xmlString = xmlString.replace(/(<[pP][^>]+)D([^a-zA-Z])/, '$1dd$2').toLowerCase();
 	return new Promise((resolve, reject) => {
-		parseString(xmlString.toLowerCase(),{explicitChildren:true,preserveChildrenOrder:true},(err,xml) => {
+		parseString(xmlString,{explicitChildren:true,preserveChildrenOrder:true},(err,xml) => {
 			if (err) return reject(err);
 			resolve(drawXml(xml));
 		});
